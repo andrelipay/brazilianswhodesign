f43a1261d0b73c242f6f4ea66ff48745b3a03d4a
andrelipay <86451259+andrelipay@users.noreply.github.com>
2021-06-24 23:15:17 -0300

    Update designers.js

--- pages/api/designers.js
+++ pages/api/designers.js
@@ -1,7 +1,7 @@
 import drive from "drive-db";
 
 export default async (req, res) => {
-  const db = await drive("12LLA-NoHin0zQfmpEblgMjd260bmriLMowBAH1QDOhI");
+  const db = await drive("1MRgRl8glCKNxk32yHdzou1kpFQn_79_65IeU2m0xNWY");
   let sanitizeResult = db.filter(
     (item) => item.name != "" && item.show == "Yes"
   );
